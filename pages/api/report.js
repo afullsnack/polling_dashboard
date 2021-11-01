36740b7b7eff41d2fa047d57c0706bb36a45b930
Mimi <miraclef60@gmail.com>
2021-11-01 18:57:21 +0100

    New endpoints added for adding report and result img to system

---
+++ pages/api/report.js
@@ -0,0 +1,75 @@
+import handler from "../../lib/handler";
+import Votes from "../../models/Votes";
+
+export default handler
+  .put(async (req, res) => {
+    try {
+      const { report, unit } = req.body;
+
+      //Update the unit vote count with data received from client
+      const votes = await Votes.updateOne(
+        {
+          "WARDS.PUs.UNIT": unit,
+        },
+        {
+          $push: {
+            "WARDS.$.PUs.$[i].REPORT": report,
+          },
+        },
+        {
+          arrayFilters: [
+            {
+              "i.UNIT": unit,
+            },
+          ],
+        }
+      );
+      if (votes.acknowledged && votes.modifiedCount >= 1) {
+        res.status(200).json({
+          error: false,
+          message: "Incident report updated successfully",
+          data: null,
+        });
+        return;
+      }
+      res.status(401).json({
+        error: true,
+        message: "Something went wrong trying to update incident report",
+        data: null,
+      });
+    } catch (e) {
+      console.log(e);
+      throw new Error(e.message || e.toString());
+    }
+  })
+  .get(async (req, res) => {
+    // get all the users for the dashboard
+
+    console.log("Params or Query Data", req.query, req.params);
+    const { unit } = req.query;
+
+    if (unit) {
+      console.log("Unit for report", unit);
+      const doc = await Votes.findOne({ "WARDS.PUs.UNIT": unit }, [
+        "WARDS.PUs.REPORT",
+      ]);
+      console.log("Return data", doc);
+      votes
+        ? res.status(200).json({
+            error: false,
+            message: "Report found",
+            data: doc,
+          })
+        : res.status(401).json({
+            error: true,
+            message: "Report not found",
+            data: null,
+          });
+    } else {
+      res.status(401).json({
+        error: true,
+        message: "Polling unit not provided",
+        data: null,
+      });
+    }
+  });
---
+++ pages/api/result_img.js
@@ -0,0 +1,43 @@
+import handler from "../../lib/handler";
+import Votes from "../../models/Votes";
+
+export default handler.put(async (req, res) => {
+  try {
+    const { unit, img, lat, lng } = req.body;
+
+    //Update the unit vote count with data received from client
+    const votes = await Votes.updateOne(
+      {
+        "WARDS.PUs.UNIT": unit,
+      },
+      {
+        $push: {
+          "WARDS.$.PUs.$[i].RESULT_IMG": { url: img, lat: lat, lng: lng },
+        },
+      },
+      {
+        arrayFilters: [
+          {
+            "i.UNIT": unit,
+          },
+        ],
+      }
+    );
+    if (votes.acknowledged && votes.modifiedCount >= 1) {
+      res.status(200).json({
+        error: false,
+        message: "Result image saved successfully",
+        data: null,
+      });
+      return;
+    }
+    res.status(401).json({
+      error: true,
+      message: "Something went wrong trying to save result report",
+      data: null,
+    });
+  } catch (e) {
+    console.log(e);
+    throw new Error(e.message || e.toString());
+  }
+});
