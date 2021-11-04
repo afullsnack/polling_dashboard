60bb7920631561024fc4f20fde93a6e857f1050f
Mimi <miraclef60@gmail.com>
2021-11-04 10:39:36 +0100

    Refactored to fix route mismatch

--- pages/api/votes.js
+++ pages/api/votes.js
@@ -1,5 +1,4 @@
 import handler from "../../lib/handler";
-import initDB from "../../lib/initDB";
 import Votes from "../../models/Votes";
 
 export default handler
@@ -107,8 +106,8 @@ export default handler
             data: null,
           });
     }
-  })
-  .post(async (req, res) => {
-    await initDB();
-    res.status(200).send("All went well");
   });
+// .post(async (req, res) => {
+//   await initDB();
+//   res.status(200).send("All went well");
+// });
