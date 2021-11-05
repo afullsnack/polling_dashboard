f24b4cb4e2497d49362b31a95aa8414e22b62501
Mimi <miraclef60@gmail.com>
2021-11-05 14:32:24 +0100

    Fixed user api route causing other routes to fail

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,35 +1,63 @@
-import handler from "../../lib/handler";
 import User from "../../models/User";
 
-export default handler
-  .post(async (req, res) => {
-    try {
-      const { name, email, phone } = req.body;
+export default async (req, res) => {
+  switch (req.method) {
+    case "POST":
+      try {
+        const { name, email, phone } = req.body;
 
-      const newUser = User({
-        name,
-        email,
-        phone,
-      });
+        const newUser = User({
+          name,
+          email,
+          phone,
+        });
 
-      await newUser.save();
-      res.status(201).json({
-        error: false,
-        message: "Created successfully",
-        data: newUser,
+        await newUser.save();
+        res.status(201).json({
+          error: false,
+          message: "Created successfully",
+          data: newUser,
+        });
+      } catch (e) {
+        console.log(e);
+        res.sendStatus(405).json({
+          error: true,
+          message: "An error occurred trying to save user",
+          data: e.message || e.toString(),
+        });
+      }
+      break;
+    case "GET":
+      // get all the users for the dashboard
+      try {
+        const { email } = req.query;
+        console.log("Email", email);
+        const users = email
+          ? await User.findOne({ email })
+          : await User.find({});
+
+        res.json({
+          error: users ? false : true,
+          message: users
+            ? "Found users"
+            : "No users found, something went wrong",
+          data: users || null,
+        });
+      } catch (e) {
+        console.log(e);
+        res.json({
+          error: true,
+          message: "An error occurred trying to get users",
+          data: e.message || e.toString(),
+        });
+      }
+      break;
+    default:
+      res.json({
+        error: true,
+        message: `Method ${req.method} not allowed`,
+        data: null,
       });
-    } catch (e) {
-      console.log(e);
-      throw new Error(e.message || e.toString());
-    }
-  })
-  .get(async (req, res) => {
-    // get all the users for the dashboard
-    const users = await User.find({});
-    console.log("Users", err || found);
-    res.json({
-      error: users ? false : true,
-      message: users ? "Found users" : "No users found, something went wrong",
-      data: users || null,
-    });
-  });
+      break;
+  }
+};
