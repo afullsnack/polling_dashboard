ed6a33d09dd4be8f6c5ddbb63abbf11cc7ae908d
Mimi <miraclef60@gmail.com>
2021-11-05 17:07:00 +0100

    Update CORS config for next.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,27 @@ require("dotenv").config();
 
 module.exports = {
   env: {},
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   webpack: (config) => {
     config.resolve.alias["components"] = path.join(__dirname, "components");
     config.resolve.alias["public"] = path.join(__dirname, "public");
